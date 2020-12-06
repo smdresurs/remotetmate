4e85e613f1896d2a0466d0fd5dc2d89a1f590f0d
smdresurs <36859960+smdresurs@users.noreply.github.com>
2020-12-07 01:12:28 +0300

    Update remotetmate.sh

--- remotetmate.sh
+++ remotetmate.sh
@@ -4,7 +4,7 @@ tmate -S /tmp/tmate.sock kill-server
 sleep 3
 tmate -S /tmp/tmate.sock new-session -d
 sleep 3
-tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' > /home/ss/s/link.txt
+tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' > /home/link.txt
 sleep 1
 echo ""
 echo "See link for connection"
