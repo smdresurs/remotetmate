16e530edbb852e0389d720473512c7f07d262ac0
smdresurs <36859960+smdresurs@users.noreply.github.com>
2020-12-07 01:10:27 +0300

    Add files via upload

---
+++ remotetmate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Создает ссылку на удаленное подключение через ssh https://tmate.io
+tmate -S /tmp/tmate.sock kill-server
+sleep 3
+tmate -S /tmp/tmate.sock new-session -d
+sleep 3
+tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' > /home/ss/s/link.txt
+sleep 1
+echo ""
+echo "See link for connection"
+tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
+echo ""
